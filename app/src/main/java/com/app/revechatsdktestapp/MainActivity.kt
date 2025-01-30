00247417da508915c707a56574e4c06c1875b7af
Saugatha Das <saugatha@revesoft.com>
2025-01-30 17:33:11 +0600

    get global update new version

--- app/src/main/java/com/app/revechatsdktestapp/MainActivity.kt
+++ app/src/main/java/com/app/revechatsdktestapp/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.app.revechatsdktestapp
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.revesoft.revechatsdk.model.VisitorInfo
 import com.revesoft.revechatsdk.service.REVEChatApiService
-import com.revesoft.revechatsdk.state.LoginState
 import com.revesoft.revechatsdk.ui.activity.ReveChatActivity
 import com.revesoft.revechatsdk.utils.ReveChat
-import com.revesoft.revechatsdk.webrtc.WebRTCHandler
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,25 +25,47 @@ class MainActivity : AppCompatActivity() {
     private lateinit var userNameEditText: EditText
     private lateinit var emailIdEditText: EditText
     private lateinit var phoneNumberEditText: EditText
-    private lateinit var loginStateCheckBox : CheckBox
+
+
+    private val accountId = "2552651"
+    private val userName = "androidSDK"
+    private val userEmail = "androidSDK@test.com"
+    private val userPhone = "01814123123"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1001
+                )
+            }
+        }
+
         accountIdEditText = findViewById(R.id.accountID)
+        accountIdEditText.setText(accountId)
+        accountIdEditText.isEnabled = false
         userNameEditText = findViewById(R.id.userName)
+        userNameEditText.setText(userName)
         emailIdEditText = findViewById(R.id.userEmail)
+        emailIdEditText.setText(userEmail)
         phoneNumberEditText = findViewById(R.id.userPhone)
-        loginStateCheckBox = findViewById(R.id.login_state_checkbox)
+        phoneNumberEditText.setText(userPhone)
+
+
 
         findViewById<Button>(R.id.startChat).setOnClickListener {
             startChat()
         }
 
-        findViewById<Button>(R.id.logOut).setOnClickListener {
-            logout()
-        }
+        // this need to be called
+        initiateReveChat()
     }
 
     override fun onDestroy() {
@@ -53,13 +75,34 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun logout() {
-//        val intent = Intent(REVEChatApiService.REVECHAT_SDK_INTENT_FILTER)
-//        intent.putExtra(REVEChatApiService.LOGOUT_MESSAGE, "")
-//        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    private fun initiateReveChat() {
+        // test access token value
+        val accessToken =
+            "Basic eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHRlcm5hbF9zeXN0ZW1fZW1haWwiOiJ0ZXN0cUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiIiLCJsYXN0TmFtZSI6IiIsImNvbXBhbnlfaWQiOjExMjksInVzZXJfbmFtZSI6InRlc3RxQGdtYWlsLmNvbSIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE3NDM4MjYyNDQsImF1dGhvcml0aWVzIjpbImNyZXciXSwianRpIjoiNjIwZmY2OWEtYjc3OS00ZjgzLThkMTEtOTA5MzBhYjc2OGMzIiwiY2xpZW50X2lkIjoid2ViIn0.QNGrQeEDUQyhAyqrg9SASVXxHxcbq62gNxR73-CsrTnn_lyufdoHr8-5UHQyrXO4ddUq9S3DNonp8VgrdiuH_oR2dwTZFy9mmwT7RbXcL33lke1hBitCIVLMXR8qyNPfBtRXTYgKYvbQVFu9hA3nlMBj4iFQb8VpAgYCo2isuFDdsTyV1T8Oevd40aecrP1PAImKVdNQVr9zJ9d-mhIrreo-4lxuhUC5cbHw1sC7xQkwk5rLWP8cMvxEBxizZVtuAoFsnjZTasMZXl6VMk61RR77fRpfbmWvmeG2FlQOiTBqlejh1Hv74ykuB9J0O18yTBKHxcwBuJUwACL81gWUOg"
+
+        ReveChat.init(accountId)
+
+        //create visitor info
+        val visitorInfo: VisitorInfo = VisitorInfo.Builder()
+            .name(userName) // set name
+            .email(userEmail)
+            .phoneNumber(userPhone)
+            .build()
+
+        visitorInfo.accessToken = accessToken // set access token if any
+
+        ReveChat.setVisitorInfo(visitorInfo)
+
+        ReveChat.setAppBundleName(BuildConfig.APPLICATION_ID)
+        ReveChat.setAppVersionNumber(BuildConfig.VERSION_NAME)
+        startService(Intent(this, REVEChatApiService::class.java))
     }
 
+
+
     private fun startChat() {
+        val accessToken = "Basic eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHRlcm5hbF9zeXN0ZW1fZW1haWwiOiJ0ZXN0cUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiIiLCJsYXN0TmFtZSI6IiIsImNvbXBhbnlfaWQiOjExMjksInVzZXJfbmFtZSI6InRlc3RxQGdtYWlsLmNvbSIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE3NDM4MjYyNDQsImF1dGhvcml0aWVzIjpbImNyZXciXSwianRpIjoiNjIwZmY2OWEtYjc3OS00ZjgzLThkMTEtOTA5MzBhYjc2OGMzIiwiY2xpZW50X2lkIjoid2ViIn0.QNGrQeEDUQyhAyqrg9SASVXxHxcbq62gNxR73-CsrTnn_lyufdoHr8-5UHQyrXO4ddUq9S3DNonp8VgrdiuH_oR2dwTZFy9mmwT7RbXcL33lke1hBitCIVLMXR8qyNPfBtRXTYgKYvbQVFu9hA3nlMBj4iFQb8VpAgYCo2isuFDdsTyV1T8Oevd40aecrP1PAImKVdNQVr9zJ9d-mhIrreo-4lxuhUC5cbHw1sC7xQkwk5rLWP8cMvxEBxizZVtuAoFsnjZTasMZXl6VMk61RR77fRpfbmWvmeG2FlQOiTBqlejh1Hv74ykuB9J0O18yTBKHxcwBuJUwACL81gWUOg"
+
         val accountId = accountIdEditText.text.toString()
         val name = userNameEditText.text.toString()
         val email = emailIdEditText.text.toString()
@@ -72,54 +115,30 @@ class MainActivity : AppCompatActivity() {
 
         ReveChat.init(accountId)
 
-
-//        var loginState : LoginState = LoginState.LOGGED_OUT
-//        var doNotShowPreChatForm = true  // dummy variable to demonstrate showing and not showing pre-chat form
-//
-//        /**
-//         * if application don't need to show pre-chat form then need to set as
-//         *      loginState = LoginState.LOGGED_IN
-//         */
-//        if (doNotShowPreChatForm)
-//            loginState = LoginState.LOGGED_IN
-
-
-        var loginState : LoginState = LoginState.LOGGED_OUT
-//        var doNotShowPreChatForm = true  // dummy variable to demonstrate showing and not showing pre-chat form
-
-        if (loginStateCheckBox.isChecked)
-            loginState = LoginState.LOGGED_IN
-
-
-        val accessToken = "Basic eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHRlcm5hbF9zeXN0ZW1fZW1haWwiOiJ0ZXN0cUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiIiLCJsYXN0TmFtZSI6IiIsImNvbXBhbnlfaWQiOjExMjksInVzZXJfbmFtZSI6InRlc3RxQGdtYWlsLmNvbSIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE3NDM4MjYyNDQsImF1dGhvcml0aWVzIjpbImNyZXciXSwianRpIjoiNjIwZmY2OWEtYjc3OS00ZjgzLThkMTEtOTA5MzBhYjc2OGMzIiwiY2xpZW50X2lkIjoid2ViIn0.QNGrQeEDUQyhAyqrg9SASVXxHxcbq62gNxR73-CsrTnn_lyufdoHr8-5UHQyrXO4ddUq9S3DNonp8VgrdiuH_oR2dwTZFy9mmwT7RbXcL33lke1hBitCIVLMXR8qyNPfBtRXTYgKYvbQVFu9hA3nlMBj4iFQb8VpAgYCo2isuFDdsTyV1T8Oevd40aecrP1PAImKVdNQVr9zJ9d-mhIrreo-4lxuhUC5cbHw1sC7xQkwk5rLWP8cMvxEBxizZVtuAoFsnjZTasMZXl6VMk61RR77fRpfbmWvmeG2FlQOiTBqlejh1Hv74ykuB9J0O18yTBKHxcwBuJUwACL81gWUOg"
-
         val visitorInfo: VisitorInfo = VisitorInfo.Builder()
             .name(name)
             .email(email)
             .phoneNumber(phone)
             .accessToken(accessToken)
-            .appLoginState(loginState)
             .build()
 
 
         ReveChat.setVisitorInfo(visitorInfo)
 
-        val intent = Intent(this, ReveChatActivity::class.java)
-        resultLauncher.launch(intent)
-//      startActivity(Intent(this, ReveChatActivity::class.java))
+//      val intent = Intent(this, ReveChatActivity::class.java)
+//      resultLauncher.launch(intent)
+        startActivity(Intent(this, ReveChatActivity::class.java))
         startService(Intent(this, REVEChatApiService::class.java))
-
     }
 
 
-    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-//          val resultData = result.data?.getStringExtra("key")
-            Toast.makeText(this, "RESULT_OK", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Operation Canceled", Toast.LENGTH_SHORT).show()
-        }
-    }
+//    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//        result ->
+//        if (result.resultCode == Activity.RESULT_OK) {
+//            Toast.makeText(this, "RESULT_OK", Toast.LENGTH_SHORT).show()
+//        } else {
+//            Toast.makeText(this, "Operation Canceled", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
 }
